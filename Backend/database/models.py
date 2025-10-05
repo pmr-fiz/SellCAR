0a4303c7d953ae32c91fde99ee1be3543d099a4b
pmr-fiz (Andrey) <andriano030706@gmail.com>
2025-10-05 23:20:02 +0300

    Create models.py

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -1,5 +1,3 @@
 from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-
-
